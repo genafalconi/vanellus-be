cd9bcdc4f7af27aa0a7c15d9a3c4bf0e4133fba1
genafalconi <genaro.f@hotmail.com>
2023-11-30 00:14:15 -0300

    fixes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import type { NestExpressApplication } from '@nestjs/platform-express';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
   // Para poder subir imgs
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.useBodyParser('urlencoded');
   app.useBodyParser('json');
   app.useBodyParser('raw');
